2ccf5d7f01e5ffc8aeb8a32e30af41b9bbc751c6
Kevin Terwilliger <kjt1040@usnh.edu>
2024-04-26 01:47:43 -0400

    jsx?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 
-import App from "./src/App";
+import App from "./src/App.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
